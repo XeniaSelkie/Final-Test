b1cd3576e967be7b04a0cadb7c2085193d334a3e
Sana <mypostoffice94@gmail.com>
2023-03-14 17:31:16 +0300

    Add programm

--- Programm for Test/Program.cs
+++ Programm for Test/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"hi", "Vhagar", "pam-param-pam", ":-)", "etc", "5", "~+_%<"};
+Console.WriteLine($"Первоначальный массив: {"hi"}, {"Vhagar"}, {"pam-param-pam"}, {":-)"}, {"etc"}, {"5"}, {"~+_%<"}");
+FindThreeCharactersElement(array);
+
+void FindThreeCharactersElement(string[] array)
+{
+    Console.Write($"Массив после выборки: ");
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index].Length <= 3)
+        {
+           Console.Write($"{array[index]} "); 
+        }
+    }
+}
